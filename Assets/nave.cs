44cd2176777d159de6ed785627a2e6f5ddb5b95b
GUSTAVO ESTRAICH PEREIRA <gustavo_pereira172@estudante.sesisenai.org.br>
2026-08-20 21:53:31 -0300

    feito

--- Assets/nave.cs
+++ Assets/nave.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class nave : MonoBehaviour
 {
+    public Animator animator;
+    public Rigidbody2D rb;
+    public float thrust = 1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void handlemovement() {
+        if (Mouse.current.leftButton.isPressed){
+            animator.SetBool("isMoving", true);
+        }else if(animator.GetBool("isMoving")){
+            animator.SetBool("isMoving", false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        handlemovement();
     }
 }
