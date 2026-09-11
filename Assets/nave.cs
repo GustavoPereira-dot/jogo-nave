1b7e6b508e9b25949d5681631e5473c353395b26
GUSTAVO ESTRAICH PEREIRA <gustavo_pereira172@estudante.sesisenai.org.br>
2026-09-10 21:54:30 -0300

    ok

--- Assets/nave.cs
+++ Assets/nave.cs
@@ -3,15 +3,15 @@ using UnityEngine.InputSystem;
 public class nave : MonoBehaviour
 {
     public Animator animator;
-    public Rigidbody2D rb;
+    Rigidbody2D rb;
     public float thrustforce = 1f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-        
+        animator = GetComponent<Animator>();
+
     }
 
     private void handlemovement() {
@@ -22,8 +22,8 @@ public class nave : MonoBehaviour
             animator.SetBool("isMoving", false);
 
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-
             Vector2 direction = (mousePosition - transform.position).normalized;
+
             transform.up = direction;
             rb.AddForce(direction * thrustforce);
         }
@@ -33,5 +33,9 @@ public class nave : MonoBehaviour
     void Update()
     {
         handlemovement();
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
     }
 }
